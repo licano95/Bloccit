ebf56ad3796f8adcb77097b5709bcf132fcd3ed5
Alex Licano <licano95@gmail.com>
2015-02-17 11:39:23 -0700

    Complete Authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,18 @@
 class PostPolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      if user == nil
+        scope.none
+      elsif user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(user: user) 
+      end
+    end
+  end
+
   def index?
     true
   end
- end
+end
