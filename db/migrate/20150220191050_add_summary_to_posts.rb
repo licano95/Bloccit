e02e5d9a67256a26e01900a1258c22b1255c3211
Alex Licano <licano95@gmail.com>
2015-02-20 12:18:13 -0700

    Redo summaries

--- db/migrate/20150220191050_add_summary_to_posts.rb
+++ db/migrate/20150220191050_add_summary_to_posts.rb
@@ -1,6 +1,6 @@
 class AddSummaryToPosts < ActiveRecord::Migration
- def change
-    add_column :posts, :summary_id, :integer
+  def change
+    add_column :posts, :summary_id, :string
     add_index :posts, :summary_id
   end
 end
