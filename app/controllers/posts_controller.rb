b6e4dd838b8a373c637d4bc289acb62a0aa96a9e
Alex Licano <licano95@gmail.com>
2015-03-02 12:35:44 -0700

    Fixed Controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,18 +11,18 @@ class PostsController < ApplicationController
   end
 
   def create
-        @topic = Topic.find(params[:topic_id])
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
-     authorize @post
-
-     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :new
-     end
-   end
+    @post.topic_id = @topic.id
+    authorize @post
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to topic_post_path(@topic, @post)
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
 
   def new
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
       
      if @post.update_attributes(post_params)
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to topic_post_path(@topic, @post)
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
